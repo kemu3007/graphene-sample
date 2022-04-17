e4b795423c43f41b648dc115670ace0d6287aeea
kemu <kemu430113@gmail.com>
2022-04-17 23:59:18 +0900

    add comment

--- src/apps/schema.py
+++ src/apps/schema.py
@@ -8,17 +8,19 @@ class UserType(DjangoObjectType):
     class Meta:
         model = User
         fields = "__all__"
+        description = "Django Default User"
 
 
 class ContenttypeType(DjangoObjectType):
     class Meta:
         model = ContentType
         fields = "__all__"
+        description = "Django Contenttype"
 
 
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
-    contenttypes = graphene.List(ContenttypeType)
+    users = graphene.List(UserType, description="ユーザ情報一覧取得API", deprecation_reason="セキュリティ上存在してはいけないAPIのためdeprecated")
+    contenttypes = graphene.List(ContenttypeType, description="コンテンツタイプ一覧取得API")
 
     def resolve_users(self, info):
         return User.objects.all()
